bb956a242ab7c4acb2066ee8b5b76dcdcc31a2b8
maddymz <madhukar4u.rm@gmail.com>
2019-11-03 00:06:51 -0700

    everything except r5

--- Lab_4/activity2/index.js
+++ Lab_4/activity2/index.js
@@ -1,3 +1,6 @@
+/**
+ * 
+ */
 var dict = {
     "dictionary_name": "default",
     "entries":
@@ -23,8 +26,6 @@ var dict = {
 }
 
 var data = " Feugiat pretium nibh ipsum consequat nisl vel pretium lectus. Id consectetur purus ut faucibus. Nunc mattis enim ut tellus elementum sagittis vitae et leo. Et sollicitudin ac orci phasellus egestas tellus. Augue neque gravida in fermentum et sollicitudin ac orci phasellus. Tempus urna et pharetra pharetra massa massa ultricies mi. Nisl purus in mollis nunc sed id semper risus in. Suspendisse interdum consectetur libero id faucibus nisl tincidunt eget. Mattis molestie a iaculis at. Vel turpis nunc eget lorem. Eu scelerisque felis imperdiet proin fermentum leo vel orci. Libero enim sed faucibus turpis in eu mi bibendum. Adipiscing diam donec adipiscing tristique. Quis ipsum suspendisse ultrices gravida dictum fusce ut placerat orci. Sem nulla pharetra diam sit amet nisl suscipit adipiscing bibendum. Proin nibh nisl condimentum id venenatis a condimentum vitae.";
-var getContentElement = document.getElementById("content");
-getContentElement.innerHTML = data;
 var commentSubmitted = false;
 var userNameSubmitted = false;
 
@@ -35,53 +36,133 @@ var userNameSubmitted = false;
 //         alert(dict.entries[5].answer[0]);
 //     }, 5000);
 // }
+
+window.onload = () => {
+    userComesBack();
+}
+
+/**
+ * 
+ * @param {*} name 
+ * @param {*} userSubmitted 
+ * @param {*} nameValue 
+ */
 function setCookie(name , userSubmitted, nameValue){
     if (userSubmitted){
         document.cookie = name + "=" + nameValue;            
     }
 }
 
+/**
+ * 
+ * @param {*} cookieName 
+ */
 function getCookie(cookieName){
     var cName = cookieName + "=";
+    console.log(cName)
     var cArray = document.cookie.split(";");
+    console.log(cArray)
     for (val in cArray){
-        var c = aArray[i];
+        var c = cArray[val];
+        console.log(c)
         while(c.charAt(0) == ' '){
-            c = c.subString(1);
+            console.log("in while")
+            c = c.substring(1);
         } 
         if(c.indexOf(cName) == 0){
-            return c.subString(cName.length, c.length);
+            console.log("in indexOf")
+            console.log(c.substring(cName.length, c.length))
+            return c.substring(cName.length, c.length);
         }       
     }
-
     return "";
 }
+
+/**
+ * @method userComesBack()
+ */
+function userComesBack(){
+    console.log("function called!!")
+    var user = getCookie('username')
+    if(user !== ""){
+        alert("welcome back" + " " + user);
+        displayContent(user);
+    }
+}
+
+/**
+ * 
+ * @param {*userName} userName 
+ * @author Madhukar Raj
+ */
+function displayContent(userName){
+    console.log("inside display content")
+    var greeting = document.getElementById('greeting');
+    greeting.innerHTML = "Welcome " + userName + " " + "to the movie review system. Please enter your comments!";
+    var getContentElement = document.getElementById("content");
+    getContentElement.innerHTML = data;
+
+}
+
+/**
+ * @author Madhukar Raj
+ * @method submitUsername()
+ * This method takes displays the contet based on the user
+ */
 function submitUsername() {
     userNameSubmitted = true;
     var userName = document.getElementById('username').value;
-    var greeting = document.getElementById('greeting');
+    
     console.log("username after submit ", userName);
-    greeting.innerHTML = "Welcome " + userName + " " + "to the movie review system. Please enter your comments!";
+    if(userName != ''){
+        var savedUser = getCookie('username')
+        if(savedUser == userName){
+            userComesBack()
+        }
+        displayContent(userName);
+        setCookie("username", userNameSubmitted, userName);
 
-    setCookie("username", userSubmitted, userName);
+    }
+    
 }
-
+/**
+ * @method submitComment()
+ * @author Madhukar Raj
+ * This function parses the submitted comments
+ */
 function submitComment() {
     commentSubmitted = true;
-    var commentVlaue = document.getElementById('commentBox').value;
-    console.log("comment value ", commentVlaue);
-
-
-    for (i in dict.entries) {
-        console.log(dict.entries[i].key[i]);
-        console.log(dict.entries[i].answer[i]);
-        if (commentVlaue.includes(dict.entries[i].key[i])) {
-            console.log("true")
-            commentVlaue.replace(dict.entries[i].key[i], dict.entries[i].answer[i]);
-
-            console.log("censored", commentVlaue);
+    var commentValue = document.getElementById('commentBox').value;
+    console.log("comment value ", commentValue);
+    var cleanString = commentValue.replace(/\n+/g, " ");
+    var commentArr = cleanString.split(' ');
+    console.log(commentArr);
+    commDict = {}
+    for (i in commentArr){
+        commDict[i] = commentArr[i]
+    }
+    if(Object.values(commDict).includes("")){
+        var arr = Object.values(commDict)
+        delete commDict[arr.indexOf("")];
+    }
+    
+    console.log(commDict);
+    var comVal = Object.values(commDict);
+    for (i in dict.entries){
+        var goodWords = dict.entries[i].answer.slice(0);
+        for (j in comVal){
+            if(dict.entries[i].key.includes(comVal[j])){
+                var replacementWrd = goodWords.pop();
+                console.log(replacementWrd)
+                var t1 = dict.entries[i].key[j]
+                var t2 = commDict[j]
+                console.log(t1, t2);
+               var censoredStr =  cleanString.replace(commDict[j], replacementWrd);
+               cleanString = censoredStr;
+            }
         }
     }
+    console.log("censored", censoredStr);
 }
 
 console.log("dictionary", dict);
